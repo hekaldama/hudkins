08374e77adf59f60a58517ad1299242131738fca
bhenderson <bhenderson@attinteractive.com>
2010-11-15 21:27:48 -0800

    Expanding cmd parser.

--- lib/hapi/command.rb
+++ lib/hapi/command.rb
@@ -5,22 +5,39 @@ class Hapi::Command < Hapi::Common
   class << self
     def run
       parse_options
+      send @command
+    end
+
+    def build
+      puts "building!!!"
+    end
+
+    def list
+      puts "listing!!!"
+    end
+
+    def config
+      puts "configing!!!"
+    end
+
+    def usage_msg
+      <<-EOB
+
+Usage: hapi [opts] commands [job_name]
+
+  Commands:   unambiguous
+  +build+::   start a job building.
+  +list+::    list jobs. [job_name used as filter].
+  +config+::  pretty print job config. job_name
+
+        EOB
     end
 
     def parse_options
       @options = {}
 
       OptionParser.new do |opts|
-        opts.banner = <<-EOB
-
-Usage: hapi [opts] commands [job_name]
-
-  Commands:
-  +build+::   start a job building.
-  +list+::    list jobs. [job_name used as filter].
-  +config+::  pretty print job config. job_name
-
-        EOB
+        opts.banner = usage_msg
 
         opts.on("-v", "--[no]-verbose", "Turn on verbose messages.") do |v|
           $VERBOSE = @options[:verbose] = v
@@ -37,7 +54,25 @@ Usage: hapi [opts] commands [job_name]
         end
       end.parse!
 
+      @cmd_list = %w(build list config)
+
       @command, @job_name = ARGV
+
+      parse_command
+    end
+
+    def parse_command
+      cmd = @cmd_list.select {|c| Regexp.new(@command, Regexp::IGNORECASE) === c}
+      case cmd.size
+      when 0 then
+        puts usage_msg
+        exit 1
+      when 1 then
+        @command = cmd.first
+      else
+        puts usage_msg
+        abort "Ambiguous command. Please specify:\n  #{cmd.join("\n  ")}\n\n"
+      end
     end
   end
 end
